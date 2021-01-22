0c8b99f952ae8984c1f99ca6c0098a0e4dbc01d0
Valet132 <aligatorr7@gmail.com>
2021-01-22 03:41:09 +0300

    Add files via upload

---
+++ Scripts/Health.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public GameObject Asteroidm;
+    public GameObject Asteroidm2;
+    public int hp = 1;
+    public bool isEnemy = true;
+    
+    public void Damage(int damageCount)
+    {
+        hp -= damageCount;
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        Shot shot = otherCollider.gameObject.GetComponent<Shot>();
+        if (shot != null)
+        {
+            if(shot.isEnemyShot != isEnemy)
+            {
+                ScoreScript.scoreValue += 50;
+                Damage(shot.damage);
+                Destroy(shot.gameObject);
+                Instantiate(Asteroidm, GetComponent<Rigidbody2D>().position, Quaternion.identity);
+                Instantiate(Asteroidm2, GetComponent<Rigidbody2D>().position, Quaternion.identity);
+            }
+        }
+        Enemyscr rot = otherCollider.gameObject.GetComponent<Enemyscr>();
+        if (rot != null)
+        {
+            if (rot.isEnemyShot == isEnemy)
+            {
+                Damage(rot.damage);
+            }
+        }
+    }
+}
