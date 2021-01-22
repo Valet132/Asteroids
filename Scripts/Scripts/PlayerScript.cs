b94b2bc052f2e7f251bac6410230b3a51b9fc756
Valet132 <aligatorr7@gmail.com>
2021-01-22 03:53:22 +0300

    Add files via upload

---
+++ Scripts/Scripts/PlayerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public Vector2 speed = new Vector2(10, 10); //Скорость
+    private Vector2 movement; //Управление
+
+
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal"); //Информация оси
+        float inputY = Input.GetAxis("Vertical");
+
+        movement = new Vector2(
+            speed.x * inputX,
+            speed.y * inputY);
+
+        bool shoot = Input.GetButtonDown("Fire1");
+        shoot |= Input.GetButtonDown("Fire2");
+
+        if(shoot)
+        {
+            WeaponScript weapon = GetComponent<WeaponScript>();
+            if (weapon != null)
+            {
+                weapon.Attack(false);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody2D>().velocity = movement;  //Перемещение
+    }
+}
